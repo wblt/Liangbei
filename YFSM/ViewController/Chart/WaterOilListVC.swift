6f37fcd1094927d56cda8dd9e7956252d774e06a
wyChris <281269704@qq.com>
2018-02-03 11:21:57 +0800

    asd

--- YFSM/ViewController/Chart/WaterOilListVC.swift
+++ YFSM/ViewController/Chart/WaterOilListVC.swift
@@ -10,23 +10,46 @@ import UIKit
 import MJExtension
 import Alamofire
 import SVProgressHUD
+import MJRefresh
 
 class WaterOilListVC: BaseVC , UITableViewDataSource, UITableViewDelegate{
 
 	var table:UITableView!
-	var data:[TimeDataModel] = [TimeDataModel]()
+	var timeDataArray:[TimeDataModel] = [TimeDataModel]()
+	var faceDataArray:[FaceDataModel] = [FaceDataModel]()
+	var page: Int = 0;
+	
+	// 顶部刷新
+	let header = MJRefreshNormalHeader()
+	// 底部刷新
+	let footer = MJRefreshAutoNormalFooter()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		table = UITableView(frame: UIScreen.main.bounds)
+		
+		self.edgesForExtendedLayout = []
+		
+		self.table = UITableView(frame: self.view.bounds, style:UITableViewStyle.grouped)
 		//设置数据源
 		self.table.dataSource = self
 		//设置代理
 		self.table.delegate = self
-		self.view.addSubview(table)
+		self.view.addSubview(self.table)
 		//注册UITableView，cellID为重复使用cell的Identifier
 		self.table.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+//		// 下拉刷新
+//		header.setRefreshingTarget(self, refreshingAction: Selector)
+//		// 现在的版本要用mj_header
+//		self.table.mj_header = header
+//
+//		// 上拉刷新
+//		footer.setRefreshingTarget(self, refreshingAction: Selector(("footerRefresh")))
+//		self.table.mj_footer = footer
 		
-		getdata()
+		self.headerRefresh()
+		self.footerRefresh()
+		
+		getdata(index: page)
         // Do any additional setup after loading the view.
     }
 	
@@ -36,7 +59,7 @@ class WaterOilListVC: BaseVC , UITableViewDataSource, UITableViewDelegate{
 
 	//设置cell的数量
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return self.data.count
+		return self.timeDataArray.count
 	}
 	
 	//设置section的数量
@@ -44,32 +67,54 @@ class WaterOilListVC: BaseVC , UITableViewDataSource, UITableViewDelegate{
 		return 1
 	}
 	
+	func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		return 0.01
+	}
+	
+	func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+		return 0.01
+	}
+	
+	func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+		return nil
+	}
+	
+	func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+		return nil
+	}
+	
 	//设置tableview的cell
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = (table.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)) as UITableViewCell
-		let model:TimeDataModel = self.data[indexPath.row]
+		let model:TimeDataModel = self.timeDataArray[indexPath.row]
 		cell.textLabel?.text = model.startTime
 		
 		return cell
 	}
 	
-	
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+		let model:TimeDataModel = self.timeDataArray[indexPath.row]
+		
+		print("起始时间：" + model.startTime)
+		print("结束时间：" + model.endTime)
+		print("数据个数：" + model.data);
+		
+	}
 	
 	// 从服务器获取
-	func getdata(){
+	func getdata(index:Int){
 		var parameters = [String: Any]()
 		BFunction.shared.showLoading()
 		let urlString = api_service + "/getmask"
 		let userDefaults = UserDefaults.standard
 		parameters["userid"] = userDefaults.value(forKey: "userid")
-		parameters["page"] = "0";
+		parameters["page"] = "\(index)";
 		parameters["pagesize"] = "10";
 		Alamofire.request(urlString, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
 			BFunction.shared.hideLoadingMessage()
+			self.table.mj_header.endRefreshing()
+			self.table.mj_footer.endRefreshing()
 			if response.error != nil  {
 				SVProgressHUD.showError(withStatus: getLocalizableString(key: "mask_data_fail", common: "获取面膜数据失败") )
 				return
@@ -82,7 +127,7 @@ class WaterOilListVC: BaseVC , UITableViewDataSource, UITableViewDelegate{
 						SVProgressHUD.showInfo(withStatus: getLocalizableString(key: "mask_data_success", common: "获取面膜数据成功") )
 						let data:Array<Dictionary> = jsonResult["data"] as! Array<Dictionary<String,Any>>;
 						//var fatherArray:[TimeDataModel] = [TimeDataModel]()
-						var sonArray:[FaceDataModel] = [FaceDataModel]()
+					//	var sonArray:[FaceDataModel] = [FaceDataModel]()
 						
 						
 						for (index,value) in data.enumerated() {
@@ -90,25 +135,10 @@ class WaterOilListVC: BaseVC , UITableViewDataSource, UITableViewDelegate{
 							print("index:\(index) = \(value)")
 							
 							let model:FaceDataModel = FaceDataModel.mj_object(withKeyValues: value);
-							sonArray.append(model);
-							
-							if sonArray.count < 5 && index != data.count-1{
-								continue;
-							}
-							
-							let timeModel:TimeDataModel = TimeDataModel()
-							let firstModel:FaceDataModel = sonArray.first!
-							let lastModel:FaceDataModel = sonArray.last!
-							
-							timeModel.startTime = firstModel.time
-							timeModel.endTime = lastModel.time
-							timeModel.data = sonArray
-							self.data.append(timeModel)
-							sonArray .removeAll()
-							
+							self.faceDataArray.append(model)
 						}
-						//获取到 cell数据
-						print(self.data)
+						
+						self.adjustData(data: self.faceDataArray)
 						self.table.reloadData()
 						
 					}else {
@@ -117,9 +147,51 @@ class WaterOilListVC: BaseVC , UITableViewDataSource, UITableViewDelegate{
 				}
 				
 			}
-			
 		}
 		
+	}
+	
+	// 将数据分组
+	func adjustData(data:[FaceDataModel]) {
+		var sonArray:[FaceDataModel] = [FaceDataModel]()
+		self.timeDataArray.removeAll()
+		
+		for (index,value) in data.enumerated() {
+			// 这样子遍历，可以得到数组的值和下标
+			print("index:\(index) = \(value)")
+			
+			sonArray.append(value);
+			if sonArray.count < 5 && index != data.count-1{
+				continue;
+			}
+			let timeModel:TimeDataModel = TimeDataModel()
+			let firstModel:FaceDataModel = sonArray.first!
+			let lastModel:FaceDataModel = sonArray.last!
+			
+			timeModel.startTime = firstModel.time
+			timeModel.endTime = lastModel.time
+			timeModel.data = sonArray
+			self.timeDataArray.append(timeModel)
+			sonArray .removeAll()
+		}
+		print(self.timeDataArray)
+		self.table .reloadData()
+	}
+	
+	func headerRefresh() {
+		self.table.mj_header = MJRefreshNormalHeader(refreshingBlock: {
+			self.page = 0
+			self.faceDataArray.removeAll()
+			self.timeDataArray.removeAll()
+			self.getdata(index: self.page)
+		})
+	}
+	
+	func footerRefresh() {
+		self.table.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: {
+			self.page = self.page + 1
+			self.getdata(index: self.page)
+		})
 	}
 	
 
